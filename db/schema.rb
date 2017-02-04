eca16348c51d1cd41994781cf23a692af18707be
Samuel Lair <lair001@gmail.com>
2017-02-04 14:26:27 -0500

    Install Teaspoon

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170115041341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        default: "", null: false
+    t.string   "email",           default: "", null: false
+    t.string   "password_digest", default: "", null: false
+    t.string   "security_code",   default: "", null: false
+    t.integer  "role",            default: 2,  null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
 
 end
