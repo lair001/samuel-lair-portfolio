1c920008475b9ec579c008da5d1517265f64b0c4
Samuel Lair <lair001@gmail.com>
2017-02-04 17:03:43 -0500

    Import regexes into Validatable

--- app/models/concerns/application_record/validatable.rb
+++ app/models/concerns/application_record/validatable.rb
@@ -2,7 +2,17 @@ class ApplicationRecord
 	module Validatable
 		module ClassMethods
 
+			def self.email_regex
+				/\A([^@]+@[^@]+\.[^.@\d]+|)\z/
+			end
 
+			def self.name_regex
+				/\A[\w'’]{,25}\z/
+			end
+
+			def self.phone_regex
+				/\A(\(\d{3}\)\d{3}-\d{4}|)\z/
+			end
 
 		end
 	end
