c668f8a8c5b5fd0f6730ea5f4437657257bb53d1
Samuel Lair <lair001@gmail.com>
2017-02-04 13:40:00 -0500

    Configure seeds.rb for data generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include FactoryGirl::Syntax::Methods
+require_all 'spec/spec_helper/config/faker'
