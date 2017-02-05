141b273dab90bedbcf732bf089fff29e1d397b59
Samuel Lair <lair001@gmail.com>
2017-02-05 16:38:55 -0500

    Pluralize route for mail#contact
    This should increase RESTfulness slightly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'visitor#index'
-	post '/mail/contact' => 'mail#contact'
+	scope '/mail' do
+		post '/contacts' => 'mail#contact', as: :mail_contacts
+	end
+
 end
