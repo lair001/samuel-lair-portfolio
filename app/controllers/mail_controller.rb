574b7aea3df3725e49bce908f97e416a85005317
Samuel Lair <lair001@gmail.com>
2017-02-05 12:53:06 -0500

    Implement mail#contact

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,7 +1,18 @@
 class MailController < ApplicationController
 
 	def contact
+		@mail_contact = MailContact.new(mail_contact_params)
+		if @mail_contact.save
+			redirect_to root_path
+		else
+			render 'visitor/index'
+		end
+	end
 
+private
+
+	def mail_contact_params
+		params.require(:mail_contact).permit(:last_name, :first_name, :email, :phone, :message)
 	end
 
 end
