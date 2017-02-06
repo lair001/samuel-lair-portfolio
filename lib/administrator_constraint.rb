813ca814de23cb35182e30e236aa4ba1e14c917b
Samuel Lair <lair001@gmail.com>
2017-02-06 12:01:06 -0500

    Fix AdministratorConstraint

--- lib/administrator_constraint.rb
+++ lib/administrator_constraint.rb
@@ -1,7 +1,7 @@
 class AdministratorConstraint
 	def matches?(request)
 		return false unless request.session[:user_id] && request.session[:security_code]
-		User.find_by(id: session[:user_id], security_code: session[:security_code])
+		user = User.find_by(id: request.session[:user_id], security_code: request.session[:security_code])
 		user && user.administrator?
 	end
 end
