a5f3b367345849378d5ed829315859cd121df338
Samuel Lair <lair001@gmail.com>
2017-02-06 09:34:01 -0500

    Import sessions controller and routes
    Mostly code from Virtual Address Book with some modifications since
    VBA's backend was strictly an API.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < JsonController
+
+	def new
+
+	end
+
+	def create
+		@user = User.find_by(username: params[:username].strip)
+		if @user && !@user.banned? && @user.authenticate(params[:password].strip)
+			redirect_to sidekiq_web_path
+		else
+			flash[:error] = "Your credentials are invalid."
+			render :new
+		end
+	end
+
+	def destroy
+		reset_session
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
 
 	mount Sidekiq::Web => '/sidekiq', :constraints => AdministratorConstraint.new
 
-
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'visitor#index'
 	scope '/mail' do
 		post '/contacts' => 'mail#contact', as: :mail_contacts
 	end
+
+	resources :sessions, only: [:new, :create, :destroy]
 
 end
